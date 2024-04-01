464af1083597dedf062473b0fc2c2984c08becd0
Ayo <45291715+p17204296@users.noreply.github.com>
2024-04-01 19:52:14 +0100

    Earnings portiom

---
+++ ayo-naitram-poc/components/EarningsAccordion.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+// import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Icon,
+  Accordion,
+  Image,
+  ChevronDownIcon,
+} from "@gluestack-ui/themed";
+import { earningsAccordionStyles } from "@/constants/styles";
+
+const Earnings = () => {
+
+  const assetsPath = "../assets/naitram";
+    // Mock earnings data
+    const earningsData = {
+      amount: "$1000",
+      currency: "MRT",
+      label: "View Earnings",
+      icon: ChevronDownIcon,
+    };
+  
+
+  return (
+    <Accordion style={earningsAccordionStyles.Accordion}>
+      <View style={earningsAccordionStyles.containerRow}>
+        <View style={earningsAccordionStyles.containerColumn}>
+          {/* Div's upper half */}
+          <View style={earningsAccordionStyles.upperHalfStyle}>
+            <Text style={earningsAccordionStyles.showRewardAmount}>{earningsData.amount}</Text>
+            <Text style={earningsAccordionStyles.showRewardCurrency}>{earningsData.currency}</Text>
+          </View>
+
+          {/* Div's lower half */}
+          <View style={earningsAccordionStyles.lowerHalfStyle}>
+            <Text style={earningsAccordionStyles.showRewardEarnings}>{earningsData.label}</Text>
+          </View>
+        </View>
+
+        <View>
+          <Image source={require(`${assetsPath}/trophy.webp`)} />
+        </View>
+      </View>
+    </Accordion>
+  );
+};
+
+export default Earnings;
--- ayo-naitram-poc/constants/styles.ts
+++ ayo-naitram-poc/constants/styles.ts
@@ -1,12 +1,23 @@
 import { StyleSheet } from "react-native";
 
+const colors = {
+  primary: "#004d40",
+  secondary: "#FAC900",
+  tertiary: "#FFA000",
+  quaternary: "#FFC107",
+  quinary: "#FFD54F",
+  senary: "#FFECB3",
+  septenary: "#FFF9C4",
+  octonary: "#FFFFFF",
+  black: "#000000",
+};
+
 export const globalStyles = StyleSheet.create({
   whiteText: {
     color: "white",
     fontSize: 24,
   },
-  container: {
-  },
+  container: {},
   title: {
     fontSize: 20,
     fontWeight: "bold",
@@ -65,7 +76,7 @@ export const headerStyles = StyleSheet.create({
     gap: 10,
     width: "33.3%",
   },
-svgContainerStyle: {
+  svgContainerStyle: {
     display: "flex",
     backgroundColor: "#004d40",
     paddingTop: 10,
@@ -76,7 +87,7 @@ svgContainerStyle: {
     borderTopRightRadius: 50,
     borderBottomRightRadius: 50,
     borderBottomLeftRadius: 50,
-},
+  },
   profileHeadingStyle: {
     color: "white",
     fontSize: 20,
@@ -89,5 +100,75 @@ svgContainerStyle: {
     fontSize: 14,
     margin: 0,
     padding: 0,
+  },
+});
+
+export const earningsAccordionStyles = StyleSheet.create({
+  Accordion: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    height: 110,
+    backgroundColor: "#FAC900", // Button background color
+    // padding: '0 20px', // Padding on the sides
+    padding: 20, // Padding on the sides
+    borderRadius: 15,
+  },
+  containerRow: {
+    display: "flex", // Enable flexbox
+    flexDirection: "row", // Arrange children in a column
+    gap: 0, // Space between items
+    textAlign: "left",
+    height: "100%",
+    flex: 1,
+  },
+  containerColumn: {
+    display: "flex", // Enable flexbox
+    flexDirection: "column", // Arrange children in a column
+    gap: 0, // Space between items
+    textAlign: "left",
+    height: "100%",
+    flex: 1,
+  },
+
+  upperHalfStyle: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "baseline", // Align the baselines of the children
+    flex: 2,
+  },
+  showRewardAmount: {
+    color: colors.black,
+    fontWeight: "800",
+    fontSize: 32,
+  },
+  showRewardCurrency: {
+    color: colors.black,
+    fontWeight: "bold",
+    marginStart: 5,
+    fontSize: 12,
+  },
+  lowerHalfStyle: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start", // Keep content at the top
+    flex: 2, // Take up available space
+  },
+  showRewardEarnings: {
+    color: colors.black,
+    fontWeight: "600",
+    fontSize: 21,
+  },
+
+  imageStyle: {
+    overflow: "hidden", // This ensures that only the part of the image fitting into the container is visible
+    display: "flex", // Use flexbox to center the image
+    justifyContent: "flex-end", // Center horizontally
+    alignItems: "center", // Center vertically
+    flex: 1,
+    aspectRatio: 1, // Maintain aspect ratio
+    height: '100%', // Set height to 100
   },
 });
